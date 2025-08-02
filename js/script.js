20bf7bd8c5f4999e6461427793eb79ceb35a3042
Weld-Brau (PTY) LTD <bonangsello1@gmail.com>
2025-08-02 10:09:07 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -343,7 +343,22 @@ document.querySelectorAll('.dropdown-menu li a').forEach(link => {
     navMenu.classList.remove('show');
   });
 });
+let lastScrollTop = 0;
+const header = document.querySelector("header");
 
+window.addEventListener("scroll", function () {
+  let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    // Scrolling down
+    header.classList.add("hidden");
+  } else {
+    // Scrolling up
+    header.classList.remove("hidden");
+  }
+
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Prevent negative scroll
+}, false);
   
 
 
